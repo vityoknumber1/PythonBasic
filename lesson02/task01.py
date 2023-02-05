1774b3db076437a5487716ca45b467dcd5fcae57
vityoknumber1 <viktorkysil1997@gmail.com>
2023-02-05 19:49:49 +0200

    First task using // and %

--- lesson02/task01.py
+++ lesson02/task01.py
@@ -1,1 +1,10 @@
-print('Hello world!')
+#Школярі та яблука
+num_p = int(input("Enter number of pupils: "))
+num_a = int(input("Enter number of apples: "))
+
+res_of_div =  num_a // num_p
+result = num_a % num_p
+
+print(f"Number of apples for each pupil: {res_of_div}")
+
+print(f"Number of offer: {result}")
